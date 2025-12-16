bc69e3091466ea1e9e2c4b6fec40c0035a785aad
Juliana Sena <julianapenhadesena31@gmail.com>
2025-12-16 16:28:28 -0300

    Create exemplo_inicial.py

---
+++ exemplo_inicial.py
@@ -0,0 +1,4 @@
+# Script inicial para o meu repositório de IA na Infinity School
+print("Olá, GitHub! Este é meu primeiro passo com Python e IA.")
+
+# Este arquivo será substituído pelo seu projeto real em breve.
